0ecfd8e6fd7254355438901f1ba210b61055b5d5
knoppf1 <kerstnerfabiano@gmail.com>
2022-06-02 11:34:50 -0300

    Lançamento

---
+++ OLA_MUNDO_JAVA/src/App.java
@@ -0,0 +1,12 @@
+import pacote1.Dog;
+import pacote2.Person;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Dog d1 = new Dog();
+        Person p1 = new Person();
+        d1.bark();
+        p1.hello();
+    }
+}
